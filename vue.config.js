83d0c871fb70b7e0785bea4967316c2fbabb0bae
Samuel <samuellemosbentosilva@gmail.com>
2024-05-27 17:19:24 -0300

    DistFixdeploy

--- vue.config.js
+++ vue.config.js
@@ -1,19 +1,4 @@
-const TerserPlugin = require('terser-webpack-plugin');
-
 module.exports = {
-  configureWebpack: {
-    optimization: {
-      minimize: true,
-      minimizer: [new TerserPlugin({
-        terserOptions: {
-          compress: {
-            drop_console: true,
-          },
-        },
-        parallel: true,
-      })],
-    },
-  },
   devServer: {
     historyApiFallback: true,
   },
