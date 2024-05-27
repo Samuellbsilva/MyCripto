91a8a9601a1ace778cbf9d19c24f5ccf3957ab8c
Samuel <samuellemosbentosilva@gmail.com>
2024-05-26 22:26:25 -0300

    FIxDeploy

--- vue.config.js
+++ vue.config.js
@@ -1,18 +1,18 @@
-module.exports = {
-    publicPath: process.env.NODE_ENV === 'production' ? '/' : '/'
-  }
   const TerserPlugin = require('terser-webpack-plugin');
 
-module.exports = {
-  configureWebpack: {
-    optimization: {
-      minimize: true,
-      minimizer: [new TerserPlugin({
-        terserOptions: {
-          // Customize Terser options here if necessary
-        },
-        parallel: true,
-      })],
+  module.exports = {
+    configureWebpack: {
+      optimization: {
+        minimize: true,
+        minimizer: [new TerserPlugin({
+          terserOptions: {
+            compress: {
+              drop_console: true,
+            },
+          },
+          parallel: true,
+        })],
+      },
     },
-  },
-};
+  };
+  
