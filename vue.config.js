175fb9ac2810ea057d95357693ffb76a168bbaed
Samuel <samuellemosbentosilva@gmail.com>
2024-05-26 23:13:19 -0300

    fix

--- vue.config.js
+++ vue.config.js
@@ -1,18 +1,28 @@
-  const TerserPlugin = require('terser-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 
-  module.exports = {
-    configureWebpack: {
-      optimization: {
-        minimize: true,
-        minimizer: [new TerserPlugin({
-          terserOptions: {
-            compress: {
-              drop_console: true,
-            },
+module.exports = {
+  configureWebpack: {
+    optimization: {
+      minimize: true,
+      minimizer: [new TerserPlugin({
+        terserOptions: {
+          compress: {
+            drop_console: true,
           },
-          parallel: true,
-        })],
-      },
+        },
+        parallel: true,
+      })],
     },
-  };
-  
+  },
+  devServer: {
+    historyApiFallback: true,
+  },
+  outputDir: 'dist',
+  publicPath: '/',
+  chainWebpack: config => {
+    config.plugin('define').tap(args => {
+      args[0]['process.env'].NODE_OPTIONS = '"--openssl-legacy-provider"';
+      return args;
+    });
+  }
+};
